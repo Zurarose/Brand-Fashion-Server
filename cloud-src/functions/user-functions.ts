0b9c5dc98518f9a0b0d5bd1ac3d28ac349fc2885
oleh.sannikov <oleh.sannikov@moonion.com>
2024-02-02 20:03:31 +0400

    feat: create custom function for gifting bonuses to users

---
+++ cloud-src/functions/user-functions.ts
@@ -0,0 +1,41 @@
+import User from '../classes/User';
+
+Parse.Cloud.define(
+  'giftBonuses',
+  async (request) => {
+    const {userId, amount, limited} = request.params;
+    const user = request.user;
+    if (!user) throw new Error('Authentication required');
+
+    const client = await new Parse.Query(User._className).equalTo('objectId', userId).first({useMasterKey: true});
+
+    if (!client) return false;
+
+    limited ? client.increment('giftedBonuses', amount) : client.increment('bonuses', amount);
+
+    if (limited) client.set('lastGiftDate', new Date());
+    await client.save(null, {useMasterKey: true});
+    return true;
+  },
+  {
+    fields: {
+      userId: {
+        required: true,
+        type: String,
+        error: 'userId is requried field',
+      },
+      amount: {
+        required: true,
+        type: Number,
+        error: 'amount number is requried field',
+      },
+      limited: {
+        required: true,
+        type: Boolean,
+        error: 'if bonuses limited is requried field',
+      },
+    },
+    requireUser: true,
+    requireAllUserRoles: ['admin'],
+  },
+);
