32a177d7f766c7a0f7cdb01087c1add4f8a976eb
oleh.sannikov <oleh.sannikov@moonion.com>
2024-02-02 20:05:34 +0400

    feat: create job to gift bonuses on user birthday

---
+++ cloud-src/jobs/checkUsersBirthday.ts
@@ -0,0 +1,38 @@
+import User from '../classes/User';
+import {ENV_VAR_NAMES} from '../lib/constants';
+
+const JOB_NAME = 'Check Limited Bonuses';
+
+Parse.Cloud.job(JOB_NAME, async () => {
+  const jobs = await new Parse.Query('_JobStatus')
+    .equalTo('jobName', JOB_NAME)
+    .equalTo('status', 'running')
+    .count({useMasterKey: true});
+  if (jobs > 1) throw Error('decline job: already running');
+
+  const currentDay = `${new Date().getDate()}/${new Date().getMonth()}`;
+
+  const config = await Parse.Config.get({useMasterKey: true});
+  const giftAmount = Number(config.get(ENV_VAR_NAMES.GIFT_BONUSES_USER_BIRTHDAY) || 0);
+
+  await new Parse.Query(User._className).each(
+    async (user) => {
+      const giftDay = user.get('lastGiftDate') || 0;
+      const lastGiftDay = `${new Date(giftDay).getDate()}/${new Date(giftDay).getMonth()}`;
+
+      if (lastGiftDay === currentDay) return;
+
+      const birthdayDate = user.get('birthday');
+      if (!birthdayDate) return;
+
+      const birthday = `${new Date(birthdayDate).getDate()}/${new Date(birthdayDate).getMonth()}`;
+      if (birthday === currentDay) {
+        user.increment('giftedBonuses', giftAmount);
+        await user.save(null, {useMasterKey: true});
+      }
+    },
+    {useMasterKey: true},
+  );
+
+  return;
+});
