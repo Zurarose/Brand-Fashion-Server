e678928b464b8b52db757a8703c9dc56d213c5e6
oleh.sannikov <oleh.sannikov@moonion.com>
2024-03-17 14:19:23 +0400

    feat: check phone unique before save new user

--- cloud-src/triggers/Client.ts
+++ cloud-src/triggers/Client.ts
@@ -4,6 +4,7 @@ import Model from '../classes/Client';
 import CloudTriggers from '../lib/CloudTriggers';
 import {ROLE_NAMES} from '../lib/constants';
 
+//SET ACL
 CloudTriggers.beforeSave(Model._className, (request) => {
   const object = request.object as Model;
   const isNew = !request.original;
@@ -13,5 +14,18 @@ CloudTriggers.beforeSave(Model._className, (request) => {
   acl.setRoleReadAccess(ROLE_NAMES.admin, true);
   acl.setRoleWriteAccess(ROLE_NAMES.admin, true);
   object.setACL(acl);
+  return true;
+});
+
+//CHECK UNIQUE PHONE
+CloudTriggers.beforeSave(Model._className, async (request) => {
+  const object = request.object as Model;
+  const isNew = !request.original;
+  if (!isNew) return false;
+
+  const phone = object.get('phone');
+  if (!phone) return false;
+  const count = await new Parse.Query(Model._className).equalTo('phone', phone).count({useMasterKey: true});
+  if (count > 0) throw new Error('User already exists');
   return true;
 });
